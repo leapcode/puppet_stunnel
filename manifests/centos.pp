65dc795b3f2ec99a08b652c2b59555235dbca48b
Gabriel Filion <gabster@lelutin.ca>
2013-04-13 00:53:00 -0400

    Variable being ignored due to incorrect quoting.

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -25,7 +25,7 @@ class stunnel::centos inherits stunnel::linux {
     source  => [ "puppet:///modules/site-stunnel/${::fqdn}/stunnel.conf",
                  "puppet:///modules/site-stunnel/${stunnel_cluster}/stunnel.conf",
                  'puppet:///modules/site-stunnel/stunnel.conf',
-                 'puppet:///modules/stunnel/${::operatingsystem}/stunnel.conf' ],
+                 "puppet:///modules/stunnel/${::operatingsystem}/stunnel.conf" ],
     require => Package['stunnel'],
     notify  => Service['stunnel'],
     owner   => root,
